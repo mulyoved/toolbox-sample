138daccfa60d2e29f751dfe512f5c75cb03663f6
Muly Oved <mulyoved@gmail.com>
2017-02-28 23:09:57 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import theme from './toolbox/theme'
+import './toolbox/theme.css'
+
+import {Button, IconButton} from 'react-toolbox/lib/button';
+
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default App;
